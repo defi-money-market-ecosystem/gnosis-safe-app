9f87eb96325b5b6235eefe57c3a4084b3b7015f1
Alaa Saleh <alaa.saleh@altoros.com>
2020-11-11 17:12:19 -0300

    UI | Mint Tab

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
-import { theme } from "@gnosis.pm/safe-react-components";
-import { Loader, Title } from "@gnosis.pm/safe-react-components";
-import SafeProvider from '@rmeissner/safe-apps-react-sdk';
+import React from "react"
+import ReactDOM from "react-dom"
+import { ThemeProvider } from "styled-components"
+import { Loader, Title } from "@gnosis.pm/safe-react-components"
+import SafeProvider from "@rmeissner/safe-apps-react-sdk"
 
-
-import GlobalStyle from "./GlobalStyle";
-import App from "./App";
+import GlobalStyle from "./GlobalStyle"
+import App from "./App"
+import { MuiThemeProvider, StylesProvider } from "@material-ui/core"
+import muiTheme from "./theme"
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <SafeProvider loading={(
-          <>
-            <Title size="md">Waiting for Safe...</Title>
-            <Loader size="md" />
-          </>
-        )}>
-        <App />
-      </SafeProvider>
-    </ThemeProvider>
+    <StylesProvider injectFirst>
+      <MuiThemeProvider theme={muiTheme}>
+        <ThemeProvider theme={muiTheme}>
+          <GlobalStyle />
+          <SafeProvider
+            loading={
+              <>
+                <Title size="md">Waiting for Safe...</Title>
+                <Loader size="md" />
+              </>
+            }
+          >
+            <App />
+          </SafeProvider>
+        </ThemeProvider>
+      </MuiThemeProvider>
+    </StylesProvider>
   </React.StrictMode>,
   document.getElementById("root")
-);
+)
